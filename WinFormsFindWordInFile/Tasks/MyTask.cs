1eef7dec2f93209c4de5326797ee4136c49f9d8c
Alla Ponurko <aponurko@gmail.com>
2022-09-29 01:17:14 +0300

    Добавьте файлы проекта.

---
+++ WinFormsFindWordInFile/Tasks/MyTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsFindWordInFile.Tasks
+{
+    public class MyTask
+    {
+        public Action FindWord => new Action(MethodFindWord);
+        public void MethodFindWord()
+        {
+
+        }
+    }
+}
